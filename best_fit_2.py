fe2453fa3adbe311bfdbe733e80f5f7190d3f4d2
Praveen Balaji <praveen@diffbot.com>
2023-12-18 13:19:47 -0800

    run2 data

--- best_fit_2.py
+++ best_fit_2.py
@@ -4,37 +4,59 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 from numpy.polynomial.polynomial import Polynomial
 
-PLOYNOMIAL_DEGREE = 2
+PLOYNOMIAL_DEGREE = 4
+concentration_vs_color = {
+'500': 'purple',
+  '1000': 'brown',
+  '1500': 'black',
+  '2000': 'red',
+  '2500': 'magenta',
+  '3000': 'green',
+  '3500': 'lime',
+  '4000': 'blue',
+  '4500': 'teal',
+  '5000': 'orange'
+}
+#colors = ['purple', 'brown', 'black', 'red', 'magenta', 'green', 'lime', 'blue', 'teal', 'orange', 'coral', 'gold', 'navy', 'olive', 'orchid', 'plum', 'salmon', 'tan', 'turquoise', 'violet', 'wheat', 'yellowgreen']
+
 def analyze_and_plot(file_names):
     plt.figure(figsize=(10, 6))
-    colors = ['red', 'green', 'blue', 'orange', 'yellow', 'purple', 'pink', 'brown', 'black', 'gray']
     color_cursor = 0
 
     for file in file_names:
+      print('Analyzing file: ', file)
       # file is of the form '2000ppm.csv'
       concentration = file.split('ppm')[0]
       data = pd.read_csv(file, header=0)
+      # data = data[data['timems'] < 125_000]
+
       timems = data['timems'].values
+      timems = timems / 1000
       voltage = data['voltage'].values
       voltage = voltage * 1000
 
       poly4 = Polynomial.fit(timems, voltage, PLOYNOMIAL_DEGREE)
 
-      # Generating a range of voltage values for plotting
-      voltage_range = np.linspace(voltage.min(), voltage.max(), 500)
-
       # Plot
-      plt.plot(timems, poly4(timems), color=colors[color_cursor], label=concentration + ' ppm')
-      print(concentration + ' ppm: ', np.poly1d(np.polyfit(timems, voltage, PLOYNOMIAL_DEGREE)))
+      color = concentration_vs_color[concentration]
+      plt.scatter(timems, voltage, color=color, s=1)
+      plt.plot(timems, poly4(timems), color=color, label=concentration + ' ppm')
 
+      # label
+      x1 = 125
+      y1 = poly4(x1) + 1
+      plt.text(x1, y1, concentration + ' ppm', color=color, va='center', ha='left')
+
+      print(concentration + ' ppm: ', np.poly1d(np.polyfit(timems, voltage, PLOYNOMIAL_DEGREE)))
       color_cursor += 1
 
-    plt.xlabel('time (millis)')
-    plt.ylabel('voltage (volts)')
+    plt.xlabel('time (seconds)')
+    plt.ylabel('voltage (mV)')
     plt.title('Degree = ' + str(PLOYNOMIAL_DEGREE))
     plt.legend()
     plt.grid(True)
     plt.show()
 
 # Usage
-analyze_and_plot(['2000ppm.csv', '3000ppm.csv', '4000ppm.csv', '5000ppm.csv'])
+# analyze_and_plot(['2000ppm.csv', '3000ppm.csv', '4000ppm.csv', '5000ppm.csv'])
+analyze_and_plot(['500ppm.csv', '1000ppm.csv', '1500ppm.csv', '2000ppm.csv', '2500ppm.csv', '3000ppm.csv', '3500ppm.csv', '4000ppm.csv', '4500ppm.csv', '5000ppm.csv'])
